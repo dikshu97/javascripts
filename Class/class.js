5a94246c882ae277799ad7cc0924db968ecff79e
dikshit <dikshitnegta32@gmail.com>
2024-04-03 09:15:50 +0530

    first day

---
+++ Class/class.js
@@ -0,0 +1,132 @@
+// let i=1;
+// while(i<=5) {
+//     for(let j=1;j<=10;j++){
+//         console.log("dikshu",i ,j);
+//     }
+//     i++;
+// }
+// console.log(("hiii"));
+// console.log("dikshu");
+// console.log(23);
+// console.log(true);
+// console.log(false);
+// console.log(null);
+// console.log(undefined);
+// process.stdout.write("hello");
+// process.stdout.write("78998");
+
+// let name="dikshu";
+// let profession="student";
+// let age=25;
+// let married=false;
+// let salary=9098;
+// let appraisal="null";
+// console.log(name);
+// console.log(profession);
+// console.log(age);
+// console.log(married);
+// console.log(salary);
+// console.log(appraisal);
+// process.stdout.write("dikshu"+"\n")
+// process.stdout.write("negta"+"\t"+"student");
+
+//
+// let age=18;
+// if (age<18){
+//     console.log("go to school");
+// }
+// if (age==18){
+//     console.log("go to hell");
+// }
+// if (age>18){
+//     console.log("dtill go to hell");
+// }
+// let age=18
+// if (age<16){
+//     console.log("go to school");
+// } else {
+//     console.log("go to college");
+// }
+// let age=20;
+// if (age<18){
+//     console.log("go to school");
+// }else{
+//     if (age==18){
+//         console.log("you are an adult");
+//     }else{
+//         if (age==20){
+//             console.log("go to date");
+//         } else {
+//             if (age<25){
+//                 console.log("get a job");
+//             } else {
+//                 if (age==30){
+//                     console.log("get married");
+//                 }
+//             }
+//         }
+//     }
+// // }
+// let age=20
+// if (age<20){
+//     console.log("go to school");
+// } else if (age==18)  {
+// console.log("you are an adult");
+// } else if (age>20){
+//     console.log("go to college");
+// } else if (age<25) {
+//     console.log("go to hell");
+// }
+// else if (age>30) {
+// console.log("get married");
+// } else {
+//     console.log("go to hell");
+// }
+// let day="Monday"
+// switch(day){
+//     case "Monday": console.log("it's monday");
+//     break;
+//     case "Tuesday": console.log("it's tuesday");
+//     break;
+//     case "Wedensday": console.log("it's wednesday");
+//     break;
+//     case "Thursday": console.log("it's thursday");
+//     break;
+//     case "Friday": console.log("it's friday");
+//     break;
+//     case "Saturday": console.log("it's saturday");
+//     break;
+//     case "Sunday": console.log("it's sunday");
+//     break;
+//     default:console.log("go to hell");
+// }
+// for(let i=1;i<=20;i++){
+//     console.log("hello dikshit"+i);
+// }
+// for (let i=0;i<=50;i++) {
+//     console.log(i);
+// }
+// let i=1;
+// while (i<=50) {
+//     console.log(i);
+//     i++;
+// }
+// let j=1;
+// while (j<=20) {
+//     console.log("dikshu"+j);
+//     j++;
+// }
+// let i=1;
+// do{
+//     console.log("dikshu"+i);
+//     i++
+// } while (i>=20) 
+    
+let i=1;
+while (i<=5) {
+    for (let j=1; j<=10; j++) {
+        console.log("dikshu",i,j);
+        
+    }
+    i++
+}console.log("bahar nikal gaya");
---
+++ home/home.js
@@ -0,0 +1,6 @@
+let age=20
+if (age<20){
+    console.log("continue your work");
+}else{
+    console.log("go to hell")
+}
