e626804ff518703f8acf599d6d67422fc770d13e
dikshit <dikshitnegta32@gmail.com>
2024-04-15 10:56:39 +0530

    commited

---
+++ home/15april/numbertraverse.js
@@ -0,0 +1,14 @@
+let num=78986;
+while(num>0){
+    let ld=num%10;
+    console.log(ld);
+    num=Math.floor(num/10)
+}
+let numa=896436788;
+nod=0;
+
+while(numa>0){
+    numa=Math.floor(numa/10);
+    nod++;
+}
+console.log("numbewr of digits are:",nod);
