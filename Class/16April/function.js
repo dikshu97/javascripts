42e007e5eefa9708ee1d468c374e3827d041d6a9
dikshit <dikshitnegta32@gmail.com>
2024-04-16 23:52:36 +0530

    commited changes of function

---
+++ Class/16April/function.js
@@ -0,0 +1,34 @@
+// function noofdigits(num) {
+//     let nod=1;
+//     while(num>0){
+//         num=Math.floor(num/10);
+//         nod++;
+//     }
+//     return nod;
+// }
+// let ans=noofdigits
+
+
+
+function saymyname(){
+    console.log("D");
+    console.log("I");
+    console.log("K");
+    console.log("S");
+    console.log("H");
+    console.log("U");
+}
+saymyname()
+saymyname()
+saymyname()
+
+
+function addition(number1,number2){
+console.log(number1+number2);
+}
+addition(5,6);
+
+function add(num1,num2){
+    console.log(num1*num2);
+}
+add(56,10);
