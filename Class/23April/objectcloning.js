7967ab8bcefbe26561191144a5406032ad48122d
dikshit <dikshitnegta32@gmail.com>
2024-04-24 12:13:25 +0530

    commited

--- Class/23April/objectcloning.js
+++ Class/23April/objectcloning.js
@@ -43,6 +43,7 @@ console.log("before",object,object2);
 object.gender="male";
 object.state="delhi";
 console.log("after",object,object2);
+//point to be noted--target=object,source=object,{x:100,y:200}=optional;
 
 //example ---2
 let ans={
@@ -59,31 +60,31 @@ console.log(ans,ans2);
 ans2.gender="Male"
 console.log(ans,ans2);
 
-//point to be noted--target=object,source=object,{x:100,y:200}=optional;
+
 
 
 //4th method=Structure cloning
-// let object1={
-//     name:"Dikshit negta",
-//     age:2883,
-//     address:"R.K.Puram",
-//     salary:1001010  
-// }
-// let newobj=structuredClone(object1);
-// console.log("before",newobj,object);
-// object1.gender="Male"
-// console.log("after",newobj,object1);
+let object1={
+    name:"Dikshit negta",
+    age:2883,
+    address:"R.K.Puram",
+    salary:1001010  
+}
+let newobj=structuredClone(object1);
+console.log("before",newobj,object);
+object1.gender="Male"
+console.log("after",newobj,object1);
 
 
-// //5th method ---object assign cloning
+//5th method ---object assign cloning
 
-// let Obj5={
-//     name:"Dikshit negta",
-//     age:2,
-//     address:"delhi",
-//     salary:1010 
-// }
-// let newobj2=Object.assign(Obj5);
-// console.log(newobj2,Obj5);
-// Obj5.gender="Male";
-// console.log(newobj2,Obj5);
+let Obj5={
+    name:"Dikshit negta",
+    age:2,
+    address:"delhi",
+    salary:1010 
+}
+let newobj2=Object.assign(Obj5);
+console.log(newobj2,Obj5);
+Obj5.gender="Male";
+console.log(newobj2,Obj5);
