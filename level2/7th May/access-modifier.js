e32815dadb5d5f7b84f1b24d4c16cab9d52e0db6
dikshit <dikshitnegta32@gmail.com>
2024-05-07 23:49:40 +0530

    commited new chnanges in the js

---
+++ level2/7th May/access-modifier.js
@@ -0,0 +1,23 @@
+class parent{
+    #firstname;
+    lastname;
+    age;
+    constructor(fname,lname,age){
+this.firstname=fname;
+this.lastname=lname;
+this.age=age;
+    }
+}
+let p1=new parent('Dikshu','Negta',27);
+console.log(p1);
+// console.log(p1.firstname);         ///public property==>>as it can b used outside object/classs
+// console.log(p1.age);
+
+class child extends parent{
+
+}
+let c1=new child('Navit','Negta',25);
+console.log(c1);
+console.log(c1.firstname);
+console.log(c1.lastname);
+console.log(c1.age);
