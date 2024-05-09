62fdeebfbced7cf6239382b3445b5cab622edc33
dikshit <dikshitnegta32@gmail.com>
2024-05-09 12:02:54 +0530

    commited the chsnges in the new file that i have made tommorrow and wnat to do it mor later to keep learning

--- level2/8th-May/object-cloning.js
+++ level2/8th-May/object-cloning.js
@@ -1,28 +1,36 @@
 //q==2 =>object cloning 
 //1st method---spread operator
 
-let obj={
-    name:"Dikshu",
-    age:27,
-    address:"karol bagh"
-}
-let obj2={...obj}
-console.log("before",obj,obj2);
-
-obj.gender="Male"
-console.log("after",obj,obj2);
 
 
-let obj={
-name:'dikshu',
-age:20,
-address:'karol bagh',
+let object1={
+    naam:"dikshu",
+    akhirinaam:"negta",
+    umar:27,
 
 }
-let obj1={
-    name:'dikshu',
-    age:26,
-    address:'delhi',
+let object2={...object1};
+console.log("before",object1,object2);
+object2.sex='male';
+console.log("after",object1,object2);
+
+
+//2nd method -- traverse
+let object3={
+    myname:"Dikshit",
+    mylastname:"Negta",
+    myage:27,
 
 }
-let
+let object4={}
+for(const key in object3){
+object4[key]=object3[key]
+}
+console.log("before",object3,object4);
+object3.mygender='female';
+console.log("after",object3,object4);
+
+
+
+
+
