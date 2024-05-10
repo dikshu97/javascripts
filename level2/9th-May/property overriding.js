b2b5ea8d7ef2f153e6748887ef7d836993b6a46d
dikshit <dikshitnegta32@gmail.com>
2024-05-10 11:27:25 +0530

    commited the changws in the file

---
+++ level2/9th-May/property overriding.js
@@ -0,0 +1,15 @@
+//property overridding
+class parent {
+    firstname="Dikshit";
+    lastname="Negta";
+    age=27;
+    sayhellotome(){
+        return "hello"+ this.firstname;
+    }
+}
+
+class child extends parent{
+firstname="puneet"
+}
+let child1=new child();
+console.log(child1.firstname);
