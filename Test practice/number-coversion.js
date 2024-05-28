f3d884d83dbb4b2b24434eeffe27e6a070af413c
dikshu97 <dikshitnegta32@gmail.com>
2024-05-29 00:39:22 +0530

    commitd the changwes on yhe file

--- Test practice/number-coversion.js
+++ Test practice/number-coversion.js
@@ -1,17 +1,17 @@
-// function anyBaseToDecimal(num, base) {
-//     let ans = 0;
-//     let power = 1;
-//     while (num > 0) {
-//         let r = num % 10;
-//         ans =ans+ (r * power);
-//         power = power*10;
-//         num = Math.floor(num / 10);
-//     }
-//     return ans;
-// }
+function anyBaseToDecimal(num, base) {
+    let ans = 0;
+    let power = 1;
+    while (num > 0) {
+        let r = num % 10;
+        ans =ans+ (r * power);
+        power = power*10;
+        num = Math.floor(num / 10);
+    }
+    return ans;
+}
 
-// let ans = anyBaseToDecimal(675477,8);
-// console.log(ans);
+let ans = anyBaseToDecimal(675477,8);
+console.log(ans);
 
 
 
@@ -31,5 +31,22 @@ function decimaltoanybase(num,base){
 }
 
 
-let ans= decimaltoanybase(51,2);
-console.log(ans);
+let ans1= decimaltoanybase(51,2);
+console.log(ans1);
+
+class baap{
+    firstname;
+    lastname;
+    age;
+    constructor(fname,lname,age){
+     this.firstname=fname;
+     this.lastname=lname;
+     this.age=age
+    }
+}
+class beta extends baap{
+
+}
+let ansss= new beta('navit','negta',20);
+console.log(ansss);
+ 
